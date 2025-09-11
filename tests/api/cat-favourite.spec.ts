aae21131ff8a3c35659153890fb37a49a142aeab
Hropt <lorvind@gmail.com>
2025-09-11 17:55:56 +0200

    PW-1 - get favourite cats

--- tests/api/cat-favourite.spec.ts
+++ tests/api/cat-favourite.spec.ts
@@ -7,5 +7,13 @@ test.describe('check that cat is', () => {
     // get random cat
     const getCat = await request.get(`${ baseUrl }images/search`)
     expect(getCat.ok()).toBeTruthy()
+    const cat = await getCat.json()
+    
+    // get favourite cats
+    const favourites = await request.get(`${ baseUrl }favourites/`, {
+      headers: {
+        "x-api-key": `${process.env.CAT_API_TOKEN}`
+      },
+    });
   });
 });
